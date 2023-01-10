a46aee312b4fe3170dcf8adcbeca9bd8a8ce01cf
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-01-10 07:19:44 -0800

    dev: checkpoint

--- src/scripts/components/SimpleModal.js
+++ src/scripts/components/SimpleModal.js
@@ -14,36 +14,36 @@
 
 // Class - SimpleModal
 export default class SimpleModal {
-	constructor(el) {
-		this.HTML = document.querySelector('html');
-		this.modalCTA = document.querySelector(`[${el}]`);
-		this.modal = document.getElementById(this.modalCTA.dataset.modal);
-		this.modalCloseBtn = this._modal.querySelector('[data-modal-close]');
-		this.isModalOpen = false;
+    constructor(el) {
+        this.HTML = document.querySelector('html');
+        this.modalCTA = document.querySelector(`[${el}]`);
+        this.modal = document.getElementById(this.modalCTA.dataset.modal);
+        this.modalCloseBtn = this._modal.querySelector('[data-modal-close]');
+        this.isModalOpen = false;
 
-		this.bindEvents();
-	}
+        this.bindEvents();
+    }
 
-	openModal() {
-		this.modal.classList.add('modal-open');
-		this.HTML.style.overflow = 'hidden';
-		this.isModalOpen = true;
-	}
+    openModal() {
+        this.modal.classList.add('modal-open');
+        this.HTML.style.overflow = 'hidden';
+        this.isModalOpen = true;
+    }
 
-	closeModal() {
-		this.modal.classList.remove('modal-open');
-		this.html.style.overflow = 'auto';
-		this.isModalOpen = false;
-	}
+    closeModal() {
+        this.modal.classList.remove('modal-open');
+        this.html.style.overflow = 'auto';
+        this.isModalOpen = false;
+    }
 
-	bindEvents() {
-		this.modalCTA.addEventListener('click', (e) => {
-			e.preventDefault();
-			this.openModal();
-		});
+    bindEvents() {
+        this.modalCTA.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.openModal();
+        });
 
-		this.modalCloseBtn.addEventListener('click', () => {
-			this.closeModal();
-		});
-	}
+        this.modalCloseBtn.addEventListener('click', () => {
+            this.closeModal();
+        });
+    }
 }
--- src/scripts/modules/NoJSModule.js
+++ src/scripts/modules/NoJSModule.js
@@ -2,43 +2,43 @@
 
 // Class - NoJSModule
 export default class NoJSModule {
-	constructor(options) {
-		this.HTML = document.querySelector('html');
+    constructor(options) {
+        this.HTML = document.querySelector('html');
 
-		if (options && options.hasOwnProperty('enable') && options.enable === true) {
-			this.enable();
-			return;
-		}
+        if (options && options.hasOwnProperty('enable') && options.enable === true) {
+            this.enable();
+            return;
+        }
 
-		this.disable();
-	}
-	
-	get isEnabled() {
-		return !this.HTML.classList.contains('no-js') ? true : false;
-	};
+        this.disable();
+    }
+    
+    get isEnabled() {
+        return !this.HTML.classList.contains('no-js') ? true : false;
+    };
 
-	get isTouchEnabled() { 
-		return !!(( 'ontouchstart' in window ) ||  
-			( window.DocumentTouch && document instanceof window.DocumentTouch) ||
-			( navigator.maxTouchPoints > 0 ) || 
-			( navigator.msMaxTouchPoints > 0 )); 
-	};
+    get isTouchEnabled() { 
+        return !!(( 'ontouchstart' in window ) ||  
+            ( window.DocumentTouch && document instanceof window.DocumentTouch) ||
+            ( navigator.maxTouchPoints > 0 ) || 
+            ( navigator.msMaxTouchPoints > 0 )); 
+    };
 
-	enable() {
-		if (this.isEnabled) {
-			this.HTML.classList.remove('js');
-			this.HTML.classList.add('no-js');
-		}
-	};
+    enable() {
+        if (this.isEnabled) {
+            this.HTML.classList.remove('js');
+            this.HTML.classList.add('no-js');
+        }
+    };
 
-	disable() {
-		if (!this.isEnabled) {
-			this.HTML.classList.remove('no-js');
-			this.HTML.classList.add('js');
-		}
+    disable() {
+        if (!this.isEnabled) {
+            this.HTML.classList.remove('no-js');
+            this.HTML.classList.add('js');
+        }
 
-		if (this.isTouchEnabled) {
-			this.HTML.classList.add('has-touch');
-		}
-	};
+        if (this.isTouchEnabled) {
+            this.HTML.classList.add('has-touch');
+        }
+    };
 }
