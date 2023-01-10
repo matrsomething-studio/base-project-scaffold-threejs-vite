0849f67d818ad41f5c65ceb764ca89f34d6eb5dd
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-01-10 15:25:12 -0800

    dev: checkpoint

--- src/scripts/components/SimpleModal.js
+++ src/scripts/components/SimpleModal.js
@@ -2,35 +2,35 @@
 
 // Class - SimpleModal
 export default class SimpleModal {
-    constructor(el) {
-        this.HTML = document.querySelector('html');
-        this.modalCTA = document.querySelector(`[${el}]`);
-        this.modal = document.getElementById(this.modalCTA.dataset.modal);
-        this.modalCloseBtn = this.modal.querySelector('[data-modal-close]');
-        this.isModalOpen = false;
+    constructor(selector) {
+        this.html = document.querySelector('html');
+        this.openBtn = document.querySelector(`[${selector}]`);
+        this.modal = document.getElementById(this.openBtn.dataset.modal);
+        this.closeBtn = this.modal.querySelector('[data-modal-close]');
+        this.isOpen = false;
 
         this.bindEvents();
     }
 
     open() {
-        this.HTML.style.overflow = 'hidden';
+        this.html.style.overflow = 'hidden';
         this.modal.classList.add('modal-open');
-        this.isModalOpen = true;
+        this.isOpen = true;
     }
 
     close() {
-        this.HTML.style.overflow = 'auto';
+        this.html.style.overflow = 'auto';
         this.modal.classList.remove('modal-open');
-        this.isModalOpen = false;
+        this.isOpen = false;
     }
 
     bindEvents() {
-        this.modalCTA.addEventListener('click', (e) => {
+        this.openBtn.addEventListener('click', (e) => {
             e.preventDefault();
             this.open();
         });
 
-        this.modalCloseBtn.addEventListener('click', () => {
+        this.closeBtn.addEventListener('click', () => {
             this.close();
         });
     }
