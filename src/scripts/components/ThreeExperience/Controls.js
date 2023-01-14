d51bd7e6914853bac3ec18dcc1ef86f5ded92388
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-01-13 18:02:34 -0800

    dev: checkpoint

---
+++ src/scripts/components/ThreeExperience/Controls.js
@@ -0,0 +1,23 @@
+
+// Control(s)
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Components(s)
+import ThreeObjects from './Objects';
+
+// Class - ThreeControls - https://threejs.org/docs/?q=OrbitControls#examples/en/controls/OrbitControls
+export default class ThreeControls extends ThreeObjects  {
+    constructor(options) {
+        super(options);
+        this.options = options;
+        
+        this.createOrbit();
+    }
+
+    createOrbit() {
+        if (this.options.orbitControls) {
+            this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+            this.controls.enableDamping = true;
+        }
+    }
+}
--- src/scripts/components/ThreeExperience/index.js
+++ src/scripts/components/ThreeExperience/index.js
@@ -1,108 +1,21 @@
-// Doc(s) - https://threejs.org/ & https://r105.threejsfundamentals.org/
-import * as THREE from 'three';
-
 // Components(s)
-import ThreeRenderer from './Renderer';
-import ThreeDataGUI from './DataGUI';
-
-// Control(s) -  https://threejs.org/docs/?q=OrbitControls#examples/en/controls/OrbitControls
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import ThreeControls from './Controls';
 
 // GSAP - https://greensock.com/docs/v3/GSAP/Timeline
 import { gsap, Quad } from 'gsap';
 
-// Shader(s)
-import fragmentRGB from '../../shaders/rgb/fragment.glsl';
-
 // Class - ThreeRenderer - https://threejs.org/docs/#api/en/renderers/WebGLRenderer
-export default class ThreeExperience extends ThreeRenderer  {
+export default class ThreeExperience extends ThreeControls  {
     constructor(options, items) {
         super(options);
         this.options = options;
         this.tl = gsap.timeline();
         this.playing = false;
         this.rafID = null;
-        this.textureLoader = new THREE.TextureLoader();
-        
-        this.setControls();
-        this.setMaterials();
-        this.setObjects();
+
         this.bindEvents();
         this.resize();
         this.play();
-    }
-
-    setControls() {
-        if (this.options.orbitControls) {
-            this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-            this.controls.enableDamping = true;
-        }
-    }
-
-    setObjects() {
-        // Plane
-        const planeGeo = new THREE.PlaneGeometry(1, 1, 1, 1);
-        const plane = new THREE.Mesh(planeGeo, this.materials.rgb);
-
-        this.scene.add(plane);
-        this.meshes.push(plane);
-
-        // Plane Instance
-        const planeInstance = new THREE.InstancedMesh(planeGeo, this.materials.rgb, 1);
-        planeInstance.position.y += -1.05;
-
-        this.scene.add(planeInstance);
-        this.meshes.push(planeInstance);
-
-        // BoxGeometry
-        const boxGeo = new THREE.BoxGeometry( 1, 1, 1 );
-        const box = new THREE.Mesh(boxGeo, this.materials.rgb);
-        box.position.x -= 1.5;
-
-        this.scene.add(box);
-        this.meshes.push(box);  
-
-        // Sphere
-        const sphereGeo = new THREE.SphereGeometry( .5, 32, 16 );
-        const texture = this.textureLoader.load('/001.jpg',
-            () =>{console.log('loading finished')},
-            () => {console.log('loading progressing')},
-            () => {console.log('loading error')}
-        );
-        const sphere = new THREE.Mesh(sphereGeo, new THREE.MeshStandardMaterial( { map: texture }));
-        sphere.position.y += 1;
-
-        this.scene.add(sphere);
-        this.meshes.push(sphere);  
-
-         // Light
-        const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-        this.scene.add( directionalLight );
-        this.lights.push(directionalLight);  
-
-        if (this.options.showGUI) {
-            this.gui = new ThreeDataGUI(this);
-        }
-    }
-
-    setMaterials() {
-        let material = new THREE.ShaderMaterial({
-            extensions: {
-                derivatives: '#extension GL_OES_standard_derivatives : enable'
-            },
-            side: THREE.DoubleSide,
-            uniforms: {
-                iTime: { value: 0 },
-                iResolution:  { value: new THREE.Vector3() },
-                iMouse: { value: this.mouse }
-            },
-            // wireframe: true,
-            // vertexShader: vertex,
-            transparent: true,
-            fragmentShader: fragmentRGB
-        });
-
-        this.materials.rgb = material;
     }
 
     resize() {
@@ -135,10 +48,7 @@ export default class ThreeExperience extends ThreeRenderer  {
         }
 
         if (this.materials) {
-            for (const [key, value] of Object.entries(this.materials)) {
-                value.uniforms.iResolution.value.set(this.width, this.height, 1);
-                value.uniforms.iTime.value = this.time.elapsed;
-            }
+            this.updateMaterials();
         }
 
         if(this.meshes) {
